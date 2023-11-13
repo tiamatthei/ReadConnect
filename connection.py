3dba5f37c6842ca3a72a3ddeb27efd5133dad281
Jorge <coke200898@gmail.com>
2023-11-13 00:22:44 -0300

    Fixed oversight with dot_env

--- connection.py
+++ connection.py
@@ -1,18 +1,16 @@
 
-from dotenv import dotenv_values
+import os
 from psycopg2 import pool
-
-secrets = dotenv_values(".env")
 
 class ConnectionPool:
     def __init__(self):
         self.connection_pool = pool.SimpleConnectionPool(
             1,  # minconn
             10,  # maxconn
-            host=secrets["DB_HOST"],
-            database=secrets["DB_NAME"],
-            user=secrets["DB_USER"],
-            password=secrets["DB_PASSWORD"]
+            host=os.environ["DB_HOST"],
+            database=os.environ["DB_NAME"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASSWORD"]
         )
 
     def get_connection(self) -> pool.SimpleConnectionPool:
